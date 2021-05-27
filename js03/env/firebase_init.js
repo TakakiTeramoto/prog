551848e7c379083177dce4b87c88d511627a3881
TakakiTeramoto <60529582+TakakiTeramoto@users.noreply.github.com>
2021-05-28 00:26:52 +0900

    4回目の授業課題

---
+++ js03/env/firebase_init.js
@@ -0,0 +1,17 @@
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAJJUhT6Kg4KKx-w6A1qilnnJo7KzxMsl8",
+    authDomain: "gschat-6b216.firebaseapp.com",
+    databaseURL: "https://gschat-6b216-default-rtdb.firebaseio.com",
+    projectId: "gschat-6b216",
+    storageBucket: "gschat-6b216.appspot.com",
+    messagingSenderId: "929268706037",
+    appId: "1:929268706037:web:57c7b0b2a0aadd03f2b3bc",
+    measurementId: "G-1PF18CS81L"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
